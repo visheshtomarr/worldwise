537128cd5f3c54a1af1ad68af7de8ad05f770b39
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-23 15:49:15 +0530

    Implemented nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          {/* This is the default route that will be display the component
+              when the "/app" url is hit. */}
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
