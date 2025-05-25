4512ceb03f62bd40b90ffaa8c80198664335e95f
visheshtomarr <visheshtomar7800@gmail.com>
2025-05-25 16:20:20 +0530

    Added map and marker for cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,37 @@
 import { useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      Map
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
